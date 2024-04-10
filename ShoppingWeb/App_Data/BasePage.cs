077f54a9fb5c39b92bbca30386df0ad0fb7493f2
I552\Administrator <aa>
2024-04-10 18:06:52 +0800

    把原本寫在Utility.cs的共用方法寫在BasePage.cs中，之後會把Utility.cs刪除，都把共用方法寫在BasePage.cs。

--- ShoppingWeb/App_Data/BasePage.cs
+++ ShoppingWeb/App_Data/BasePage.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Web;
+using System.Web.Services;
 
 namespace ShoppingWeb.Ajax
 {
@@ -13,5 +16,90 @@ namespace ShoppingWeb.Ajax
         }
 
         //可把原本寫在Uyility的共用方法寫在這
+
+        /// <summary>
+        /// 確認是否有重複登入
+        /// </summary>
+        /// <returns></returns>
+        [WebMethod]
+        public static bool CheckDuplicateLogin()
+        {
+            bool result = false;
+            try
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings["cns"].ConnectionString;
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand cmd = new SqlCommand("pro_sw_getSessionId", con))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        con.Open();
+                        cmd.Parameters.Add(new SqlParameter("@userId", HttpContext.Current.Session["userId"]));
+
+                        object dbResult = cmd.ExecuteScalar();
+
+                        if (dbResult != null)
+                        {
+                            HttpContext.Current.Session["dbID"] = dbResult.ToString();
+
+                            string currentSessionID = HttpContext.Current.Session.SessionID;
+                            string dbSessionID = HttpContext.Current.Session["dbID"].ToString();
+
+                            if (dbSessionID == currentSessionID)
+                            {
+                                result = true;
+                            }
+                            else
+                            {
+                                HttpContext.Current.Session["userId"] = null;
+                            }
+                        }
+
+                        return result;
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Exception: " + ex.Message);
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 將 DataTable 轉換為 JSON 字串的輔助方法
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        public static string ConvertDataTableToJson(DataTable dt)
+        {
+            System.Web.Script.Serialization.JavaScriptSerializer serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
+            System.Collections.ArrayList rows = new System.Collections.ArrayList();
+            System.Collections.IDictionary row;
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                row = new Dictionary<string, object>();
+                foreach (DataColumn col in dt.Columns)
+                {
+                    row.Add(col.ColumnName, dr[col]);
+                }
+                rows.Add(row);
+            }
+
+            return serializer.Serialize(rows);
+        }
+
+        /// <summary>
+        /// 判斷權限是否可使用該功能
+        /// </summary>
+        /// <param name="roles"></param>
+        /// <returns></returns>
+        public static bool CheckRoles(int roles)
+        {
+            return ((int)HttpContext.Current.Session["roles"] == 1 || (int)HttpContext.Current.Session["roles"] == roles);
+        }
+
     }
 }
