dd08266f54debe9564692ba0812b351232721a11
I552\Administrator <aa>
2024-04-15 11:49:42 +0800

    訂單總攬。

--- ShoppingWeb/Ajax/OrderHandler.aspx.cs
+++ ShoppingWeb/Ajax/OrderHandler.aspx.cs
@@ -1,16 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Services;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
 namespace ShoppingWeb.Ajax
 {
-    public partial class OrderHandler : System.Web.UI.Page
+    public partial class OrderHandler : BasePage
     {
-        protected void Page_Load(object sender, EventArgs e)
+        private const int PERMITTED_USER_ROLES = 3;
+
+        /// <summary>
+        /// 一開始顯示所有會員資訊
+        /// </summary>
+        /// <returns></returns>
+        [WebMethod]
+        public static object GetAllOrderData()
         {
+            if (!CheckDuplicateLogin())
+            {
+                return "重複登入";
+            }
+
+            if (!CheckRoles(PERMITTED_USER_ROLES))
+            {
+                return "權限不足";
+            }
+
+            string connectionString = ConfigurationManager.ConnectionStrings["cns"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("pro_sw_getAllOrderData", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    con.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    DataTable dt = new DataTable();
+                    dt.Load(reader);
+
+                    // 將資料轉換為 JSON 格式返回
+                    return ConvertDataTableToJson(dt);
+                }
+            }
 
         }
     }
--- ShoppingWeb/js/OrderManagement.js
+++ ShoppingWeb/js/OrderManagement.js
@@ -1,4 +1,91 @@
-﻿$(document).ready(function () {
-    window.parent.getUserPermission();
+﻿// 訂單狀態
+let orderStatus = {
+    "1": { name: "處理中", color: "bg-warning" },
+    "2": { name: "已確認", color: "bg-success" },
+    "3": { name: "已完成", color: "bg-white" },
+    "4": { name: "已取消", color: "bg-danger" }
+};
 
-})
+// 付款狀態
+let paymentStatus = {
+    "1": { name: "未付款", color: "bg-warning" },
+    "2": { name: "付款失敗", color: "bg-danger" },
+    "3": { name: "超過付款時間", color: "bg-danger" },
+    "4": { name: "已付款", color: "bg-success" },
+    "5": { name: "退款中", color: "bg-warning" },
+    "6": { name: "已退款", color: "bg-danger" }
+};
+
+// 配送狀態
+let deliveryStatus = {
+    "1": { name: "備貨中", color: "bg-warning" },
+    "2": { name: "發貨中", color: "bg-warning" },
+    "3": { name: "已發貨", color: "bg-success" },
+    "4": { name: "已到達", color: "bg-white" },
+    "5": { name: "已取貨", color: "bg-white" },
+    "6": { name: "已退回", color: "bg-danger" },
+    "7": { name: "退回中", color: "bg-warning" }
+};
+
+// 配送方式
+let deliveryMethod = {
+    "1": "超商取貨",
+    "2": "店到店",
+    "3": "宅配"
+};
+
+
+$(document).ready(function () {
+    window.parent.getUserPermission();
+    SearchAllOrder();
+
+
+});
+
+//全部訂單資料
+function SearchAllOrder() {
+    $.ajax({
+        url: '/Ajax/OrderHandler.aspx/GetAllOrderData',
+        type: 'POST',
+        contentType: 'application/json',
+        success: function (response) {
+            if (response.d == "重複登入") {
+                alert("重複登入，已被登出");
+                window.parent.location.href = "Login.aspx";
+            } else if (response.d === "權限不足") {
+                alert("權限不足");
+                parent.location.reload();
+            } else {
+                let data = JSON.parse(response.d);
+                let tableBody = $('#tableBody');
+
+                tableBody.empty();
+
+                $.each(data, function (index, item) {
+                    let row = '<tr>' +
+                        '<td>' + item.f_id + '</td>' +
+                        '<td>' + item.f_memberId + '</td>' +
+                        '<td>' + item.f_createdTime + '</td>' +
+                        '<td>' +
+                        '<span class="px-2 rounded ' + orderStatus[item.f_orderStatus].color + '">' + orderStatus[item.f_orderStatus].name + '</span>' +
+                        '</td>' +
+                        '<td>' +
+                        '<span class="px-2 rounded ' + paymentStatus[item.f_paymentStatus].color + '">' + paymentStatus[item.f_paymentStatus].name + '</span>' +
+                        '</td>' +
+                        '<td>' +
+                        '<span class="px-2 rounded ' + deliveryStatus[item.f_deliveryStatus].color + '">' + deliveryStatus[item.f_deliveryStatus].name + '</span>' +
+                        '</td>' +
+                        '<td>' + deliveryMethod[item.f_deliveryMethod] + '</td>' +
+                        '<td>' + item.f_total + '</td>' +
+                        '</tr>';
+
+                    tableBody.append(row);
+                });
+            }
+
+        },
+        error: function (error) {
+            console.error('Error:', error);
+        }
+    });
+}
