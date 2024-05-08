1e2cf40ee6cf46c6a998bafe8c1b4b3f9e43490e
I552\Administrator <aa>
2024-05-08 16:55:38 +0800

    分頁按鈕一律顯示五個，當前頁數和前後兩頁。

--- ShoppingWeb/js/v1000/UserManagement.js
+++ ShoppingWeb/js/v1000/UserManagement.js
@@ -1,5 +1,5 @@
 ﻿let currentPage = 1; // 當前頁數，初始頁數為 1
-let pageSize = 5; // 每頁顯示的資料筆數
+let pageSize = 1; // 每頁顯示的資料筆數
 let pagesTotal = null; //總頁數
 
 $(document).ready(function () {
@@ -116,18 +116,34 @@ function SearchAllUserInfo(pageNumber, pageSize) {
                     tableBody.append(row);
                 });
 
-                //依資料筆數來開分頁頁數
-                if (response.d.TotalPages > 0) {
+                //依資料筆數來開分頁頁數 
+                if (pagesTotal > 0) {
                     let ulPagination = $('#ulPagination');
                     ulPagination.empty();
 
-                    paginationBtnHtml = '<li class="page-item" id="firstPage"><a class="page-link" href="#">' + langFont["firstPage"] + '</a></li>' +
-                        '<li class="page-item" id="previousPage"><a class="page-link" href="#"> << </a></li>';
-                    for (let i = 1; i <= response.d.TotalPages; i++) {
-                        paginationBtnHtml += '<li class="page-item" id="page' + i + '"><a class="page-link pageNumber" href="#">' + i + '</a></li>';
+                    paginationBtnHtml = '<li class="page-item" id="firstPage"><a class="page-link" href="#"> << </a></li>' +
+                        '<li class="page-item" id="previousPage"><a class="page-link" href="#"> < </a></li>';
+
+                    if (currentPage <= 2) {
+                        // 當前分頁是前兩頁之一，生成後幾頁
+                        for (let i = 1; i <= Math.min(5, pagesTotal); i++) {
+                            paginationBtnHtml += '<li class="page-item" id="page' + i + '"><a class="page-link pageNumber" href="#">' + i + '</a></li>';
+                        }
+                    } else if (currentPage >= pagesTotal - 1) {
+                        // 當前分頁是最後兩頁之一，生成前幾頁
+                        for (let i = Math.max(1, pagesTotal - 4); i <= pagesTotal; i++) {
+                            paginationBtnHtml += '<li class="page-item" id="page' + i + '"><a class="page-link pageNumber" href="#">' + i + '</a></li>';
+                        }
+                    } else {
+                        // 其他情況，生成當前分頁和前後兩頁的按鈕
+                        for (let i = currentPage - 2; i <= currentPage + 2; i++) {
+                            paginationBtnHtml += '<li class="page-item" id="page' + i + '"><a class="page-link pageNumber" href="#">' + i + '</a></li>';
+                        }
                     }
-                    paginationBtnHtml += '<li class="page-item" id="nextPage"><a class="page-link" href="#"> >> </a></li>' +
-                        '<li class="page-item" id="lastPage"><a class="page-link" href="#"> ' + langFont["lastPage"] + ' </a></li>';
+
+                    paginationBtnHtml += '<li class="page-item" id="nextPage"><a class="page-link" href="#"> > </a></li>' +
+                        '<li class="page-item" id="lastPage"><a class="page-link" href="#"> >> </a></li>';
+
                     ulPagination.append(paginationBtnHtml);
                 }
             }
