544c5f90f1a8fcccbc2f88f5e0d154184d0b72f9
I552\Administrator <aa>
2024-06-26 17:35:42 +0800

    小更改

--- ShoppingWeb/js/v1000/sw/components/FrameComponent.js
+++ ShoppingWeb/js/v1000/sw/components/FrameComponent.js
@@ -1,25 +1,25 @@
 ﻿var FrameComponent = {
     template: `
         <div class="col-12 col-md-10">
-            <component :is='pageName'></component>
+            <component :is='name'></component>
         </div>
     `,
     data: function () {
         return {
-            pageName: 'default-component',
+            name: 'default-component',
         }
     },
     methods: {
         //顯示選擇的頁面
-        ShowPage: function (val) {
-            this.pageName = val;
+        Show: function (val) {
+            this.name = val;
         }
     },
     created: function () {
-        this.$bus.$on('Frame:Change', this.ShowPage);
+        this.$bus.$on('Frame:Change', this.Show);
     },
     beforeDestroy: function () {  //組件銷毀前，移除該監聽
-        this.$bus.$off('Frame:Change', this.ShowPage);
+        this.$bus.$off('Frame:Change', this.Show);
     },
     components: {
         'default-component': DefaultComponent,
