17dd76b01c0e8d2c05f370a47d039eefdf76fe57
I552\Administrator <aa>
2024-07-09 12:03:31 +0800

    order單元測試(初版)

---
+++ ShoppingWeb/Repository/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingWeb.Repository
+{
+    public interface IOrderRepository : IBaseRepository
+    {
+        (Exception, int?) EditOrder(EditOrderDto dto);
+    }
+}
---
+++ ShoppingWeb/Repository/OrderRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingWeb.Repository
+{
+    public class OrderRepository : BaseRepository, IOrderRepository
+    {
+        /// <summary>
+        /// 更改訂單
+        /// </summary>
+        /// <param name="dto"></param>
+        /// <returns></returns>
+        public (Exception, int?) EditOrder(EditOrderDto dto)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand cmd = new SqlCommand("pro_sw_editOrderData", con))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        con.Open();
+                        cmd.Parameters.Add(new SqlParameter("@orderId", dto.OrderId));
+                        cmd.Parameters.Add(new SqlParameter("@orderStatus", dto.OrderStatusNum));
+                        cmd.Parameters.Add(new SqlParameter("@deliveryStatus", dto.DeliveryStatusNum));
+                        cmd.Parameters.Add(new SqlParameter("@deliveryMethod", dto.DeliveryMethodNum));
+
+                        return (null, (int)cmd.ExecuteScalar());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return (ex, null);
+            }
+        }
+    }
+}
